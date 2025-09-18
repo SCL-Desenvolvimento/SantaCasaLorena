2cba2bb2ebe106d253bcbce7887d52505e1280c6
Rossini <42848119+RossiniADS@users.noreply.github.com>
2025-09-18 16:10:29 -0300

    Update environment.ts

--- santacasalorena.client/src/environments/environment.ts
+++ santacasalorena.client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44369/api',
-  imageServerUrl: 'https://localhost:44369/'
+  apiUrl: 'https://localhost:7072/api',
+  imageServerUrl: 'https://localhost:7072/'
 };
